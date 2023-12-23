285095de6ab301d12667182162f4ca002d4cdf01
Yongjun Chai <yongjunchai@hotmail.com>
2023-12-23 11:20:55 -0800

    implement PR and RPN visitor

--- java/lox/src/main/java/com/craftinginterpreters/lox/AstPrinterPolishNotation.java
+++ java/lox/src/main/java/com/craftinginterpreters/lox/AstPrinterPolishNotation.java
@@ -1,6 +1,6 @@
 package com.craftinginterpreters.lox;
 
-public class AstPrinter implements  Expr.Visitor<String>{
+public class AstPrinterPolishNotation implements  Expr.Visitor<String>{
     String print(Expr expr) {
         return expr.accept(this);
     }
@@ -37,14 +37,39 @@ public class AstPrinter implements  Expr.Visitor<String>{
         return builder.toString();
     }
 
-    public static void main(String[] args) {
-        Expr expression = new Expr.Binary(
+    private static Expr binaryExpr() {
+        return new Expr.Binary(
                 new Expr.Unary(new Token(TokenType.MINUS, "-", null, 1),
-                new Expr.Literal(123)),
-        new Token(TokenType.STAR, "*", null, 1),
-        new Expr.Grouping(new Expr.Literal(45.67)
-        ));
-        System.out.println(new AstPrinter().print(expression));
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(new Expr.Literal(45.67)
+                ));
+    }
+
+    private static Expr complexExpr() {
+        return new Expr.Binary(
+                new Expr.Binary(new Expr.Literal( "1"),
+                     new Token(TokenType.AND, "+", null, 1),
+                        new Expr.Literal("2")
+                        ),
+
+
+
+                new Token(TokenType.STAR, "*", null, 1),
+
+
+
+                new Expr.Binary(new Expr.Literal("4"),
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal( "3")
+                )
+        );
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new AstPrinterPolishNotation().print(binaryExpr()));
+        System.out.println(new AstPrinterPolishNotation().print(complexExpr()));
     }
 
 }
